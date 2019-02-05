c4cf4e1f013ce6fbe61a837df7eede0217258166
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-02-05 20:04:45 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,17 @@ public class Main {
     public static void main(String[] args) {
         int firstYear = 1997;
         int lastYear = Calendar.getInstance().get(Calendar.YEAR);
-        int passportQuantity = 10;
-        //25 - Иркутская обл., 50 - Новосибирская обл., 69 - Томская обл.
-        int[] regionCodes = {25, 50, 69};
-        double[][] passportSeries = new double[regionCodes.length][lastYear - firstYear];
+        //25 - Иркутская обл., 50 - Новосибирская обл., 69 - Томская обл., 01 - Алтайский край
+        String[] regionCodes = {"25", "50", "69", "01"};
+        String[] years = new String[lastYear-firstYear];
+        String[][] passportSeries = new String[regionCodes.length][lastYear - firstYear];
 
         for (int i=0; i<regionCodes.length; i++){    //Кол-во строк матрицы = кол-во регионов
             System.out.println("");
-            for (int j=0; j<passportQuantity; j++){  //Кол-во столбцов = кол-во лет в период с firstYear до lastYear
-                passportSeries[i][j] = Math.random();
-                System.out.print(passportSeries[i][j]+" ");
+            for (int j=0; j<lastYear-firstYear; j++){  //Кол-во столбцов = кол-во лет в период с firstYear до lastYear
+                years[j] = Integer.toString(firstYear + j);
+                passportSeries[i][j] = regionCodes[i] + years[j].substring(years[j].length() - 2);
+                System.out.format(passportSeries[i][j] + " ");
             }
         }
     }
